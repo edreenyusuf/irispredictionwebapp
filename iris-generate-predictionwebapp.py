2f95a5af3a1d35c2e121d95ee7945ed3d4c8d021
edreenyusuf <154412074+edreenyusuf@users.noreply.github.com>
2023-12-22 16:03:26 +0800

    Update iris-generate-predictionwebapp.py

--- iris-generate-predictionwebapp.py
+++ iris-generate-predictionwebapp.py
@@ -25,7 +25,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = pickle.load(open("irispredictionmodel.h5", "rb"))
+loaded_model = pickle.load(open("irispredictionmodelv3", "rb"))
 
 prediction = loaded_model.predict_proba(df)
 
